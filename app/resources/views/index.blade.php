bd99be525863fb2a3a47d227f11e5bb6b4cc2736
ybahadi <yassinebahadi04@gmail.com>
2024-05-20 18:45:55 +0200

    bug front end fix

--- app/resources/views/index.blade.php
+++ app/resources/views/index.blade.php
@@ -44,34 +44,34 @@
     <div class="container-fluid p-0 bg-light">
         <!-- Navbar Start -->
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark px-4 px-lg-5 py-3 py-lg-0">
-    <a href="index.php" class="navbar-brand p-0 ">
-        <h1 class="text-primary m-0 d-flex">
-            <img src="./img/logo.svg" alt="Logo" class=" d-inline-block h-100 w-auto">
-            <span class=" h-100 align-bottom align-self-end">Ressine</span>
-        </h1>
-    </a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
-        <span class="fa fa-bars"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarCollapse">
-        <div class="navbar-nav ms-auto py-0 pe-4">
-            <a href="index.php" class="nav-item nav-link active">Home</a>
-            <a href="about.php" class="nav-item nav-link">About</a>
-            <a href="service.php" class="nav-item nav-link">Service</a>
-            <a href="menu.php" class="nav-item nav-link">Menu</a>
-            <div class="nav-item dropdown">
-                <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Pages</a>
-                <div class="dropdown-menu m-0">
-                    <a href="booking.php" class="dropdown-item active">Booking</a>
-                    <a href="team.php" class="dropdown-item">Our Team</a>
-                    <a href="testimonial.php" class="dropdown-item">Testimonial</a>
+            <a href="index.php" class="navbar-brand p-0 ">
+                <h1 class="text-primary m-0 d-flex">
+                    <img src="./img/logo.svg" alt="Logo" class=" d-inline-block h-100 w-auto">
+                    <span class=" h-100 align-bottom align-self-end">Ressine</span>
+                </h1>
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+                <span class="fa fa-bars"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarCollapse">
+                <div class="navbar-nav ms-auto py-0 pe-4">
+                    <a href="index.php" class="nav-item nav-link active">Home</a>
+                    <a href="about.php" class="nav-item nav-link">About</a>
+                    <a href="service.php" class="nav-item nav-link">Service</a>
+                    <a href="menu.php" class="nav-item nav-link">Menu</a>
+                    <div class="nav-item dropdown">
+                        <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Pages</a>
+                        <div class="dropdown-menu m-0">
+                            <a href="booking.php" class="dropdown-item active">Booking</a>
+                            <a href="team.php" class="dropdown-item">Our Team</a>
+                            <a href="testimonial.php" class="dropdown-item">Testimonial</a>
+                        </div>
+                    </div>
+                    <a href="contact.php" class="nav-item nav-link">Contact</a>
                 </div>
+                <a href="booking.php" class="btn btn-primary py-2 px-4 rounded-full">Book A Table</a>
             </div>
-            <a href="contact.php" class="nav-item nav-link">Contact</a>
-        </div>
-        <a href="booking.php" class="btn btn-primary py-2 px-4 rounded-full">Book A Table</a>
-    </div>
-</nav>
+        </nav>
         <!-- Navbar End -->
 
 
@@ -128,7 +128,8 @@
             </div>
 
             <div class="buttons">
-                <button id="prev" class=" wow fadeInRight"><</button>
+                <button id="prev" class=" wow fadeInRight">
+                    <</button>
                         <button id="next" class=" wow fadeInLeft">></button>
             </div>
         </div>
@@ -490,55 +491,55 @@
         <!-- panorama end -->
         <!-- footer start -->
         <div class="container-fluid bg-dark text-light footer pt-5 mt-5 wow fadeIn" data-wow-delay="0.1s">
-    <div class="container py-5">
-        <div class="row g-5">
-            <div class="col-lg-3 col-md-6">
-                <h1 class="text-primary m-0 d-flex">
-                    <img src="./img/logo.svg" alt="Logo" class=" d-inline-block h-100 w-auto">
-                    <span class=" h-100 align-bottom align-self-end">Ressine</span>
-                </h1>
-            </div>
-            <div class="col-lg-3 col-md-6">
-                <h4 class="section-title ff-secondary text-start text-primary fw-normal mb-4">Contact</h4>
-                <p class="mb-2"><i class="fa fa-map-marker-alt me-3"></i>123 Street, New York, USA</p>
-                <p class="mb-2"><i class="fa fa-phone-alt me-3"></i>+012 345 67890</p>
-                <p class="mb-2"><i class="fa fa-envelope me-3"></i>info@example.com</p>
-                <div class="d-flex pt-2">
-                    <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-twitter"></i></a>
-                    <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-facebook-f"></i></a>
-                    <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-youtube"></i></a>
-                    <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-linkedin-in"></i></a>
+            <div class="container py-5">
+                <div class="row g-5">
+                    <div class="col-lg-3 col-md-6">
+                        <h1 class="text-primary m-0 d-flex">
+                            <img src="./img/logo.svg" alt="Logo" class=" d-inline-block h-100 w-auto">
+                            <span class=" h-100 align-bottom align-self-end">Ressine</span>
+                        </h1>
+                    </div>
+                    <div class="col-lg-3 col-md-6">
+                        <h4 class="section-title ff-secondary text-start text-primary fw-normal mb-4">Contact</h4>
+                        <p class="mb-2"><i class="fa fa-map-marker-alt me-3"></i>123 Street, New York, USA</p>
+                        <p class="mb-2"><i class="fa fa-phone-alt me-3"></i>+012 345 67890</p>
+                        <p class="mb-2"><i class="fa fa-envelope me-3"></i>info@example.com</p>
+                        <div class="d-flex pt-2">
+                            <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-twitter"></i></a>
+                            <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-facebook-f"></i></a>
+                            <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-youtube"></i></a>
+                            <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-linkedin-in"></i></a>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6">
+                        <h4 class="section-title ff-secondary text-start text-primary fw-normal mb-4">Opening</h4>
+                        <h5 class="text-light fw-normal">Monday - Saturday</h5>
+                        <p>09AM - 09PM</p>
+                        <h5 class="text-light fw-normal">Sunday</h5>
+                        <p>10AM - 08PM</p>
+                    </div>
+                    <div class="col-lg-3 col-md-6">
+                        <h4 class="section-title ff-secondary text-start text-primary fw-normal mb-4">Newsletter</h4>
+                        <p>Dolor amet sit justo amet elitr clita ipsum elitr est.</p>
+                        <div class="position-relative mx-auto" style="max-width: 400px;">
+                            <input class="form-control border-primary w-100 py-3 ps-4 pe-5" type="text"
+                                placeholder="Your email">
+                            <button type="button"
+                                class="btn btn-primary py-2 position-absolute top-0 end-0 mt-2 me-2">SignUp</button>
+                        </div>
+                    </div>
                 </div>
             </div>
-            <div class="col-lg-3 col-md-6">
-                <h4 class="section-title ff-secondary text-start text-primary fw-normal mb-4">Opening</h4>
-                <h5 class="text-light fw-normal">Monday - Saturday</h5>
-                <p>09AM - 09PM</p>
-                <h5 class="text-light fw-normal">Sunday</h5>
-                <p>10AM - 08PM</p>
-            </div>
-            <div class="col-lg-3 col-md-6">
-                <h4 class="section-title ff-secondary text-start text-primary fw-normal mb-4">Newsletter</h4>
-                <p>Dolor amet sit justo amet elitr clita ipsum elitr est.</p>
-                <div class="position-relative mx-auto" style="max-width: 400px;">
-                    <input class="form-control border-primary w-100 py-3 ps-4 pe-5" type="text"
-                        placeholder="Your email">
-                    <button type="button"
-                        class="btn btn-primary py-2 position-absolute top-0 end-0 mt-2 me-2">SignUp</button>
+            <div class="container">
+                <div class="copyright">
+                    <div class="row">
+                        <div class="col-md-12 text-center text-md-center mb-3 mb-md-0">
+                            © 2024 Ressine - All Rights Reserved
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-    <div class="container">
-        <div class="copyright">
-            <div class="row">
-                <div class="col-md-12 text-center text-md-center mb-3 mb-md-0">
-                    © 2024 Ressine - All Rights Reserved
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
         <!-- footer end -->
     </div>
 

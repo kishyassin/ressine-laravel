d587ad94b5ab2d8b6eb8a3de4fc4f10b6102b766
KICH Yassine <kishyassin@gmail.com>
2024-05-25 20:53:30 +0100

    install laravel ide helper package

--- app/config/app.php
+++ app/config/app.php
@@ -181,6 +181,8 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
+        
 
         /*
          * Package Service Providers...

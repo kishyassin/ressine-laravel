7731923153c99fd4689518b2caa0ab80048b99ef
KICH Yassine <kishyassin@gmail.com>
2024-05-26 01:54:24 +0100

    create RelationManagers page in filament client ressources

--- app/app/Filament/Resources/ClientResource.php
+++ app/app/Filament/Resources/ClientResource.php
@@ -10,8 +10,7 @@ use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\SoftDeletingScope;
+
 
 class ClientResource extends Resource
 {
---
+++ app/app/Filament/Resources/ClientResource/RelationManagers.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Filament\Resources\ClientResource;
+
+class RelationManagers
+{
+
+}

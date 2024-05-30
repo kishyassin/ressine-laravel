3dce8d4bb42b845f09f58350ea3900eb12e27d45
ybahadi <yassinebahadi04@gmail.com>
2024-05-30 14:06:37 +0200

    change image factory

--- app/database/factories/ImageFactory.php
+++ app/database/factories/ImageFactory.php
@@ -17,7 +17,7 @@ class ImageFactory extends Factory
     public function definition(): array
     {
         return [
-            'idPlat' => \App\Models\Plat::factory(),
+            'idPlat' => fake()->unique()->numberBetween(1, 25),
             'imageHero' => fake()->imageUrl($width = 1370, $height = 770, 'food', true, 'Faker'),
             'imageIcon' => fake()->imageUrl($width = 200, $height = 200, 'food', true, 'Faker'),
             'imageSlide' => fake()->imageUrl($width = 320, $height = 480, 'food', true, 'Faker'),

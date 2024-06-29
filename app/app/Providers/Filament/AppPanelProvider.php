188a813336d06e17f0ba4e6b9dfd36d2bef73301
KICH Yassine <kishyassin@gmail.com>
2024-06-29 20:29:33 +0100

    app panel home link

--- app/app/Providers/Filament/AppPanelProvider.php
+++ app/app/Providers/Filament/AppPanelProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers\Filament;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\DisableBladeIconComponents;
 use Filament\Http\Middleware\DispatchServingFilamentEvent;
+use Filament\Navigation\NavigationItem;
 use Filament\Pages;
 use Filament\Panel;
 use Filament\PanelProvider;
@@ -49,6 +50,14 @@ class AppPanelProvider extends PanelProvider
             ])
             ->authMiddleware([
                 Authenticate::class,
+            ])
+            ->navigationItems([
+                NavigationItem::make('Retour a Ressine ')
+                    ->url('/', shouldOpenInNewTab: false)
+                    ->icon('heroicon-o-presentation-chart-line')
+                ->badge('Accueil')
+
+
             ]);
     }
 }
